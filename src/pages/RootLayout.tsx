9f0b9ad9a33e88647dbdac6906aee1e51ca0d97b
GuiHolanda <guilherme.lopes.holanda@hotmail.com>
2023-04-28 15:03:08 -0300

    removing CartTAb

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-import { CartTab } from "../components/CartTab";
 
 export const RootLayoutPage = () => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="flex flex-col px-4 container mx-auto h-full">
-        <CartTab />
         <div className="flex-grow mb-8">
           <Outlet />
         </div>
